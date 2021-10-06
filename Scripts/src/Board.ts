1ef68f3d76ff9db92152d2e8c43b5a5915668d6e
FotisSpinos <fotis.spinos23@gmail.com>
2021-10-06 14:45:11 -0400

    Refactoring and typo corrections

--- Scripts/src/Board.ts
+++ Scripts/src/Board.ts
@@ -3,8 +3,8 @@ import { IPiece } from "../Interfaces/Piece.js"
 export type BlockPosition = { x: number, y: number }
 
 export class Board {
-    private color1: string
-    private color2: string
+    private oddColor: string
+    private evenColor: string
 
     public resolution: number
     public squareSize: number
@@ -12,43 +12,29 @@ export class Board {
 
     private pieces: Map<IPiece, BlockPosition> = new Map<IPiece, BlockPosition>();
 
-    constructor(color1: string, color2: string) {
-        this.color1 = color1;
-        this.color2 = color2;
-    }
-
-    public drawBoard(ctx: CanvasRenderingContext2D, resolution: number, size: number) {
+    constructor(resolution, size) {
+        this.oddColor = 'red'
+        this.evenColor = 'blue'
         this.resolution = resolution
         this.squareSize = (size / resolution)
+    }
+
+    public setColors(oddColor: string, evenColor: string) {
+        this.oddColor = oddColor;
+        this.evenColor = evenColor;
+    }
+
+    public drawBoard(ctx: CanvasRenderingContext2D) {
         this.ctx = ctx;
 
-        let color = ''
-
-        for (let y = 0; y < resolution; y++) {
-            for (let x = 0; x < resolution; x++) {
-                if (y % 2 == 0) {
-                    if (x % 2 == 0) {
-                        color = this.color1
-                    }
-                    else {
-                        color = this.color2
-                    }
-                }
-                else {
-                    if (x % 2 == 0) {
-                        color = this.color2
-                    }
-                    else {
-                        color = this.color1
-                    }
-                }
-
-                this.drawBlockWithColor(x, y, color)
+        for (let y = 0; y < this.resolution; y++) {
+            for (let x = 0; x < this.resolution; x++) {
+                this.drawBlockWithColor(x, y, this.GetColorForBlock({x, y}))
             }
         }
     }
 
-    public addPeace(piece: IPiece, x: number, y: number) {
+    public addPiece(piece: IPiece, x: number, y: number) {
         if (!this.isValidBlock(x, y)) {
             throw Error('invalid block')
         }
@@ -56,7 +42,7 @@ export class Board {
         this.pieces.set(piece, { x, y });
     }
 
-    public setColor(x: number, y: number, color: string) {
+    public drawBlockWithColor(x: number, y: number, color: string) {
         if (this.isValidBlock(x, y)) {
             this.ctx.fillStyle = color
             this.ctx.fillRect(this.squareSize * x, this.squareSize * y, this.squareSize, this.squareSize);
@@ -66,33 +52,48 @@ export class Board {
         }
     }
 
-    public setColorToBlocks(blocks: BlockPosition[], color: string) {
+    public drawblocksWithColor(blocks: BlockPosition[], color: string) {
         blocks.forEach(x => {
-            this.setColor(x.x, x.y, color);
+            this.drawBlockWithColor(x.x, x.y, color);
         })
     }
 
     public getValidPiecePosition(piece: IPiece): BlockPosition {
-        if (this.pieces.get(piece) != null) {
-            return this.pieces.get(piece)
-        }
-        throw Error('Piece could not be found')
+        return this.pieces.get(piece);
     }
 
     public getPawnPositions(): Set<String> {
         let set = new Set<String>();
-
         this.pieces.forEach(x => set.add(JSON.stringify(x)))
 
         return set;
     }
 
-    private drawBlockWithColor(x: number, y: number, color: string) {
-        this.ctx.fillStyle = color
-        this.ctx.fillRect(this.squareSize * x, this.squareSize * y, this.squareSize, this.squareSize);
-    }
-
     private isValidBlock(x: number, y: number): boolean {
         return (x > -1 && x < this.resolution && y > -1 && y < this.resolution)
+    }
+
+    private GetColorForBlock(pos: BlockPosition): string {
+        
+        let color = ''
+
+        if (pos.y % 2 == 0) {
+            if (pos.x % 2 == 0) {
+                color = this.evenColor
+            }
+            else {
+                color = this.oddColor
+            }
+        }
+        else {
+            if (pos.x % 2 == 0) {
+                color = this.oddColor
+            }
+            else {
+                color = this.evenColor
+            }
+        }
+        
+        return color;
     }
 }

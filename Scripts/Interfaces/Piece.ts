2b3b42601c51ced29d19b08aef36010034ba5453
FotisSpinos <fotis.spinos23@gmail.com>
2021-10-17 08:50:15 -0400

    Deleted unused files

--- Scripts/Interfaces/Piece.ts
+++
@@ -1,6 +0,0 @@
-import { Board, BoardPosition } from "../src/Board.js";
-
-export interface IPiece {
-    getValidMoves(board: Board) : BoardPosition[]
-    draw(board: Board, pos: BoardPosition): void
-}
